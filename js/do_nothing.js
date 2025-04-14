17a52b5f8738c19880c07bbf7644c20cb4b76b00
x-tools-author <x-tools@outlook.com>
2025-04-14 18:55:25 +0800

    chore: update the file

---
+++ js/do_nothing.js
@@ -0,0 +1,23 @@
+﻿function x_flow_plugin_get_api_version() {
+    return "1.0";
+}
+
+function x_flow_plugin_get_caption() {
+    return "Do Nothing";
+}
+
+function x_flow_plugin_get_name() {
+    return "Do Nothing";
+}
+
+function x_flow_plugin_get_in_ports() {
+    return 1;
+}
+
+function x_flow_plugin_get_out_ports() {
+    return 1;
+}
+
+function x_flow_plugin_handle_data(str) {
+    return str;
+}
