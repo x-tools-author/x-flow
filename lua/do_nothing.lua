a917ec628f3106e1b0242bfb22647d22f8345ef5
x-tools-author <x-tools@outlook.com>
2025-04-14 19:06:06 +0800

    chore: update the file

---
+++ lua/do_nothing.lua
@@ -0,0 +1,23 @@
+﻿local function x_flow_plugin_get_api_version()
+    return "1.0";
+end
+
+local function x_flow_plugin_get_caption()
+    return "Do Nothing";
+end
+
+local function x_flow_plugin_get_name()
+    return "Do Nothing";
+end
+
+local function x_flow_plugin_get_in_ports()
+    return 1;
+end
+
+local function x_flow_plugin_get_out_ports()
+    return 1;
+end
+
+local function x_flow_plugin_handle_data(str)
+    return str;
+end
